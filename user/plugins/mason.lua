b3fde53f7c0dcf6852882cfc008d30a11c1db627
Amit Sharma <amitsharmamad@gmail.com>
2023-08-14 19:48:32 +0530

    added tsserver and jsonls lsps

---
+++ user/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "tsserver",
+        "jsonls"
+      })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = function(_, opts)
+      -- add more things to the ensure_installed table protecting against community packs modifying it
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "prettier"
+      })
+    end,
+  },
+}
